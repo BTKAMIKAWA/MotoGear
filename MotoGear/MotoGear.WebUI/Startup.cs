d21f33e34390a0200cd635a7a940001d4d162530
Brian Kamikawa <briankamikawa@gmail.com>
2019-06-21 13:18:35 -0700

    Initial Solution Setup

---
+++ MotoGear/MotoGear.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MotoGear.WebUI.Startup))]
+namespace MotoGear.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
