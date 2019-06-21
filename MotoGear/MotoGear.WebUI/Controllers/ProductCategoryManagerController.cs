fa223a65bd72dcf14aab3b777ed5a0a3776b3bcc
Brian Kamikawa <briankamikawa@gmail.com>
2019-06-21 16:47:27 -0700

    Catgory add, edit and delete functionality added

---
+++ MotoGear/MotoGear.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,114 @@
+﻿using MotoGear.Core.Models;
+using MotoGear.DataAccess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MotoGear.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        ProductCategoryRepo context;
+
+        public ProductCategoryManagerController()
+        {
+            context = new ProductCategoryRepo();
+        }
+        
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategories = context.Collection().ToList();
+            return View(productCategories);
+        }
+
+        public ActionResult Create()
+        {
+            ProductCategory productCategories = new ProductCategory();
+            return View(productCategories);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategories)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategories);
+            }
+            else
+            {
+                context.Insert(productCategories);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            ProductCategory productCategories = context.Find(Id);
+            if (productCategories == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCategories);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCategories, string Id)
+        {
+            ProductCategory productCatToEdit = context.Find(Id);
+            if (productCatToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(productCategories);
+                }
+                productCatToEdit.Category = productCategories.Category;
+      
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            ProductCategory productCatToDelete = context.Find(Id);
+            if (productCatToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCatToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            ProductCategory productCatToDelete = context.Find(Id);
+            if (productCatToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
