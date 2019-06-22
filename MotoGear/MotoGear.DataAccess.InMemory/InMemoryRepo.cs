1f6fb21a1ba5c7c8ffb9287ee575af4dfd7c4995
Brian Kamikawa <briankamikawa@gmail.com>
2019-06-22 16:02:46 -0700

    add

---
+++ MotoGear/MotoGear.DataAccess.InMemory/InMemoryRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoGear.DataAccess.InMemory
+{
+    public class InMemoryRepo<Ty>
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Ty> items;
+        string className;
+
+    }
+}
