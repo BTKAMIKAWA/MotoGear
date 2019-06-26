6042e0dc1ccde8170c8d5beb242097b587f28d05
Brian Kamikawa <briankamikawa@gmail.com>
2019-06-25 19:22:26 -0700

    Created sort by category functionality and link bar on left side of the product list and individual product description pages

--- MotoGear/MotoGear.WebUI/Controllers/HomeController.cs
+++ MotoGear/MotoGear.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MotoGear.Core.Contracts;
 using MotoGear.Core.Models;
+using MotoGear.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,10 +20,25 @@ namespace MotoGear.WebUI.Controllers
             productCategories = productCategoryContext;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(String Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if(Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
 
         public ActionResult Details(string Id)
