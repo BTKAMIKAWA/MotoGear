6507894b1dccbbf2d4073cb54906320258a8644f
Brian Kamikawa <briankamikawa@gmail.com>
2019-06-26 01:27:13 -0700

    All cart services and methods finished. Created contract referance

---
+++ MotoGear/MotoGear.Core/ViewModels/CartSummaryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoGear.Core.ViewModels
+{
+    public class CartSummaryViewModel
+    {
+        public int ShoppingCartCount { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+
+        public CartSummaryViewModel()
+        {
+
+        }
+
+        public CartSummaryViewModel(int shoppingCartCount, decimal shoppingCartTotal)
+        {
+            this.ShoppingCartCount = shoppingCartCount;
+            this.ShoppingCartTotal = shoppingCartTotal;
+
+        }
+
+    }
+}
